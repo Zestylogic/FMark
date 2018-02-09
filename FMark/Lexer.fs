ff2281a2810b29119acb0886fcd531732c79ca96
Yann Herklotz <ymherklotz@gmail.com>
2018-02-09 17:21:05 +0000

    [Lexer] Fixing the lexer.

--- FMark/Lexer.fs
+++ FMark/Lexer.fs
@@ -6,11 +6,13 @@ type LexerState = Normal | InCodeBlock
 
 type LexerData = {Source: string; State: LexerState}
 
-let nextToken source = END
+let nextToken st = END, st
 
-let tokenize st =
-    let rec tokenize' tokList st =
+let tokenize source =
+    let rec tokenize' st tokList =
         match st.Source with
         | "" -> END :: tokList
         | _ ->
-            match 
+            let nt, st' = nextToken st
+            nt :: tokList |> tokenize' st
+    tokenize' {Source=source; State=Normal} [] |> List.rev
