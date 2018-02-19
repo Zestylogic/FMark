a9a4b59be4b41e347b03b8b12c6e056bf0de6d96
Qieerb <qieerbushe7@gmail.com>
2018-02-19 17:28:31 +0000

    Should work now

--- FMark/Program.fs
+++ FMark/Program.fs
@@ -1,7 +1,8 @@
 ﻿open System
 open Types
 
-type Citation = int * Line
+// TODO: Footnote support for
+// simple format, hyperlink, picture
 
 let rec tocParse tocLst depth : THeader list =
     match tocLst with
@@ -14,23 +15,30 @@ let rec tocParse tocLst depth : THeader list =
 let tocGen tokenLst maxDepth =
     List.filter (fun x -> x.Level <= maxDepth) (tocParse tokenLst 0)
 
-let rec lineParse tocLst : Line =
-    []
+let rec footLineParse tocLst : Line =
+    match tocLst with
+    | LITERAL lit::tl -> FrmtedWordLst [lit] :: footLineParse tl
+    | ENDLINE::WHITESPACE 4::tl -> footLineParse tl
+    | ENDLINE::_ -> []
+    | _::tl -> footLineParse tl
+    | [] -> []
 
 let rec citeParse tocLst : ParsedObj list =
     match tocLst with
     | LSBRA::CARET::NUMBER key::RSBRA::tl ->
         match tl with
-        | COLON::tail -> Footnote (key, lineParse tail) :: citeParse tail
-        | tail -> citeParse tail
+        | COLON::tail -> Footnote (key, footLineParse tail) :: citeParse tail
+        | tail -> citeParse tail // TODO: do something when it is inline?
     | _::tl -> citeParse tl
     | [] -> []
 
-
-
 [<EntryPoint>]
 let main argv =
-    let testTokenLst = [HASH; HASH; LITERAL("hi"); LITERAL("this is a paragraph"); EMPTYLINE;
-                        LITERAL("Cool"); HASH; HASH; HASH; LITERAL("Another Title"); END]
-    printf "%A" (tocGen testTokenLst 2)
+    let testTokenLst = [HASH; HASH; LITERAL "hi"; LITERAL "this is a paragraph"; EMPTYLINE;
+                        LITERAL "Cool"; HASH; HASH; HASH; LITERAL "Another Title"; ENDLINE]
+    let testTokenLst2 = [LSBRA; CARET; NUMBER 1; RSBRA; COLON; LITERAL "This...text.";
+                        ENDLINE; WHITESPACE 4; LITERAL "conti...lines."; ENDLINE; 
+                        LITERAL "stop...nted."]                  
+    printf "\n%A\n" (tocGen testTokenLst 3)
+    printf "\n%A\n" (citeParse testTokenLst2)
     0
