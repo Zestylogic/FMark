e99c086a8d3c61a687c509dac71e4ac485099234
Qieerb <qieerbushe7@gmail.com>
2018-02-19 16:36:47 +0000

    Initial parsing, not working yet

--- FMark/Program.fs
+++ FMark/Program.fs
@@ -1,6 +1,36 @@
 ﻿open System
+open Types
+
+type Citation = int * Line
+
+let rec tocParse tocLst depth : THeader list =
+    match tocLst with
+    | HASH::tl -> tocParse tl (depth+1)
+    | LITERAL h::tl when depth > 1 ->
+        {HeaderName = [h]; Level = depth} :: tocParse tl 0
+    | _::tl -> tocParse tl 0
+    | [] -> []
+
+let tocGen tokenLst maxDepth =
+    List.filter (fun x -> x.Level <= maxDepth) (tocParse tokenLst 0)
+
+let rec lineParse tocLst : Line =
+    []
+
+let rec citeParse tocLst : ParsedObj list =
+    match tocLst with
+    | LSBRA::CARET::NUMBER key::RSBRA::tl ->
+        match tl with
+        | COLON::tail -> Footnote (key, lineParse tail) :: citeParse tail
+        | tail -> citeParse tail
+    | _::tl -> citeParse tl
+    | [] -> []
+
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let testTokenLst = [HASH; HASH; LITERAL("hi"); LITERAL("this is a paragraph"); EMPTYLINE;
+                        LITERAL("Cool"); HASH; HASH; HASH; LITERAL("Another Title"); END]
+    printf "%A" (tocGen testTokenLst 2)
     0
