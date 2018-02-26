60d677a7d3f0e3cbc634159e92b7bd3b5b19750d
Qieerb <qieerbushe7@gmail.com>
2018-02-26 19:58:40 +0000

    Added comments

--- FMark/Parser.fs
+++ FMark/Parser.fs
@@ -2,6 +2,7 @@ module Parser
 open Types
 
 let rec (|NormalWords|_|) tok =
+    // special characteres commented out
     match tok with
     | LITERAL lit -> Some lit
     | WHITESPACE _ -> Some " "
@@ -40,16 +41,19 @@ let rec (|NormalWords|_|) tok =
     | PERCENT -> Some "%"
     | _ -> None
 
+// start of emphasis
 let (|EmphasisS|_|) = function
     | ASTERISK :: tl -> Some tl
     | WHITESPACE _ :: UNDERSCORE :: tl -> Some tl
     | _ -> None
 
+// end of emphasis
 let (|EmphasisE|_|) = function
     | ASTERISK :: tl -> Some tl
     | UNDERSCORE :: WHITESPACE _ :: tl -> Some tl
     | _ -> None
 
+// parse all plain text at head of list
 let parseText tLst =
     let rec textIn tLst' pLst' :Token list* InlineElement list =
         match tLst' with
@@ -57,6 +61,7 @@ let parseText tLst =
         | _ -> tLst', pLst'
     textIn tLst []
 
+// parse line with formats
 let parseLine toLst =
     let rec parseLineRec pLst tLst: TLine*Token list =
         // printf "parseLine\n%A\n" tLst
@@ -75,13 +80,15 @@ let parseLine toLst =
     parseLineRec [] toLst
     |> fun (x,y) -> List.rev x, y
 
+// call this if rest of the token list not needed
 let parseLine' tLst = (fun (x,_) -> x) (parseLine tLst)
 
 // --------------------------------------------------------------------------------
 let rec tocParse tocLst depth index : THeader list * Token list =
     // Detect hashes with whitespace after it
     // printf "tocParse %A\n%A\n" depth tocLst
-    // let refFit (a,b) = {HeaderName = parseLine' h; Level = depth} :: tocParse t 0
+
+    // rebuild hash if no whitespace after
     let rec fakehash dep =
         match dep with
         | 0 -> [ENDLINE]
@@ -92,6 +99,7 @@ let rec tocParse tocLst depth index : THeader list * Token list =
     | HASH::tl when depth > 0 -> tocParse tl (depth+1) index
     | WHITESPACE _ ::tl when depth > 0 ->
         let ind = tl |> List.tryFindIndex (fun x -> x = ENDLINE)
+        //split header from rest of tokens by finding ENDLINE
         match ind with
         | Some i ->
             let (h,t) = List.splitAt i tl
@@ -99,6 +107,7 @@ let rec tocParse tocLst depth index : THeader list * Token list =
             |> fun (x,y) -> {HeaderName = parseLine' h; Level = depth}::x, HEADER index::y
         | None ->
             [{HeaderName = parseLine' tl; Level = depth}], [HEADER index]
+    //hash without whitespace, need to rebuild hash
     | a::tl when depth > 0 ->
         tocParse tl 0 index
         |> fun (x,y) -> x, List.append (fakehash depth |> List.rev) (a::y)
@@ -113,8 +122,9 @@ let tocGen' tokenLst maxDepth =
     | d when d > 0 ->
         tocParse tokenLst 0 0
         |> fun (x,y) -> List.filter (fun x -> x.Level <= d) x, y
-    | _ -> failwithf "Invalide maxDepth" // railway this? not necessary yet
+    | _ -> failwithf "Invalide maxDepth" // will railway this. not necessary yet
 
+// call this when ParsedObj wanted
 let tocGen tLst maxD =
     {MaxDepth = maxD; HeaderLst = tocGen' tLst maxD |> fun (x,_)->x}
 
@@ -133,7 +143,8 @@ let rec citeParse tocLst =
 //parse footnotes with parseLine
 and citeParseIn tLne tocLst :TLine*Token list =
     match tocLst with
-    | ENDLINE::WHITESPACE 4::tl ->
+    // continue if next line is indented
+    | ENDLINE::WHITESPACE a::tl  when a >= 4 ->
         citeParseIn tLne tl
         |> fun (x,y) -> List.append tLne x, y
     | ENDLINE::tl -> tLne, tl
@@ -171,7 +182,8 @@ let rec citeParse' tocLst :(int*TLine)list*Token list =
 //parse footnotes with parseLine
 and citeParseIn' tLne tocLst :TLine*Token list =
     match tocLst with
-    | ENDLINE::WHITESPACE 4::tl ->
+    // continue if next line is indented
+    | ENDLINE::WHITESPACE a::tl when a >= 4->
         citeParseIn' tLne tl
         |> fun (x,y) -> List.append tLne x, y
     | ENDLINE::tl -> tLne, tl
