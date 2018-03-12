cfd7a563dbaf2a721aa2f9c5b906edde0608e409
ThunderMikey <mikecyj25@gmail.com>
2018-03-12 18:03:21 +0000

    html whole doc generation (#104)
    * Adding gitattributes
    
    * add to level genHTML
    
    * change to a more exciting title

--- FMark/src/Common/HTMLGen/HTMLGenTester.fs
+++ FMark/src/Common/HTMLGen/HTMLGenTester.fs
@@ -146,6 +146,16 @@ let inlineFootnoteTests =
     ]
 
 [<Tests>]
+let ``HTML head generation test``=
+    makeExpectoTestList id id genHead "HTML head generation test" [
+        (
+            "tiny title",
+            "<head><meta name=\"viewport\" content=\"width=device-width\"><title>tiny title</title></head>",
+            "simple header"
+        );
+    ]
+
+[<Tests>]
 let TOCTests =
     let hLst1 = [{HeaderName=[FrmtedString(Literal "header1")]; Level=1}
                      ;{HeaderName=[FrmtedString(Literal "header2")]; Level=1}
@@ -255,4 +265,22 @@ let fullBodyTests =
             "<p>Go go go!<a href=\"brokenURL\">broken link</a>Come!</p>"]
             , "the bodyshop"
         );
+    ]
+
+[<Tests>]
+let ``global simple test`` =
+    makeExpectoTestList id id genHTML "top level genHTML test" [
+        ("FMarkToHtml fisrt release",
+            [
+                Header{HeaderName=[FrmtedString(Literal "header")]; Level=1};
+                List{ListType=UL;ListItem=
+                    [StringItem[FrmtedString(Literal "first")]; StringItem[FrmtedString(Literal "second")];
+                        NestedList{ListType=OL;ListItem=
+                        [StringItem[FrmtedString(Literal "first")]; StringItem[FrmtedString(Literal "second")] ];
+                        Depth=2} ];
+                    Depth=1};
+                Table[PCells([CellLine([FrmtedString(Literal "head")], true, Left);CellLine([FrmtedString(Literal "head")], true, Right)], true)];
+                Paragraph[[FrmtedString((Literal "Go go go!")); Link(Literal "broken link", "brokenURL")]; [FrmtedString(Literal "Come!")]]
+            ]),
+        "<!DOCTYPE html><head><meta name=\"viewport\" content=\"width=device-width\"><title>FMarkToHtml fisrt release</title></head><body><h1>header</h1><ul><li>first</li><li>second</li><ol><li>first</li><li>second</li></ol></ul><table><thead><tr><th align=\"left\">head</th><th align=\"right\">head</th></tr></thead><tbody></tbody></table><p>Go go go!<a href=\"brokenURL\">broken link</a>Come!</p></body>", "all in one"
     ]
