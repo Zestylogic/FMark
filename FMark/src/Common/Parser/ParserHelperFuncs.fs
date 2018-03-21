6d488687c3c8db674101bc026a70a7de492febba
ThunderMikey <mikecyj25@gmail.com>
2018-03-21 17:44:33 +0000

    footnote, citation bug fix
    append currentLine so that anything before footnote and citation are not thrown away

--- FMark/src/Common/Parser/ParserHelperFuncs.fs
+++ FMark/src/Common/Parser/ParserHelperFuncs.fs
@@ -415,14 +415,14 @@ let parseInLineElements2 refLst toks =
             let idStr = string i
             match findFN i ftLst with
             | Ok _ -> // ok if found at least one reference in refLst
-                [(Literal idStr, idStr) |> Reference], rtks
+                [(Literal idStr, idStr) |> Reference]@currentLine, rtks
             | Error msg -> // error if no reference is found in refLst
                 [msg |> Literal |> FrmtedString], rtks
         | CITATION str :: rtks ->
             match findCite str ftLst with
             | Ok ref -> // ok if found at least one reference in refLst
                 match ref with
-                | Citation (id, hyperText, _) -> [(hyperText, id) |> Reference], rtks
+                | Citation (id, hyperText, _) -> [(hyperText, id) |> Reference]@currentLine, rtks
                 | _ -> failwith "non-citation in citation list"
             | Error msg -> // error if no reference is found in refLst
                 [msg |> Literal |> FrmtedString], rtks
