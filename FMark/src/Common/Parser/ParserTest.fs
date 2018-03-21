ef540eb781d961114222ff2933d79ade38deece6
ThunderMikey <mikecyj25@gmail.com>
2018-03-21 17:25:21 +0000

    splitup the big reference test

--- FMark/src/Common/Parser/ParserTest.fs
+++ FMark/src/Common/Parser/ParserTest.fs
@@ -291,42 +291,70 @@ let testGlobal =
     ]
 
 [<Tests>]
-let testGlobal2 =
-    makeExpectoTestList id id parse "top level test part 2" [
-        (
-            [PERCENT; PERCENT; LITERAL "Style"; EQUAL; WHITESPACE 1; LITERAL "Chicago"; ENDLINE;
-                LITERAL "text1"; HASH; LITERAL "text2"; ENDLINE; ENDLINE;
-                HASH; WHITESPACE 1; LITERAL "Header1"; ENDLINE;
-                LITERAL "text3"; LSBRA; CARET; NUMBER "1"; RSBRA; LITERAL "text4"; ENDLINE; ENDLINE;
-                LSBRA; CARET; NUMBER "1"; RSBRA; COMMA; LITERAL "footer1"; ENDLINE;
-                LITERAL "text5"; LSBRA; CARET; LITERAL "Eric"; RSBRA; LITERAL "text6"; ENDLINE; ENDLINE;
-                // end of doc references
-                LSBRA; CARET; LITERAL "Eric"; RSBRA; COMMA; LITERAL "type";EQUAL; WHITESPACE 1;
+let ``reference tests`` =
+    let refToks =
+        [LSBRA; CARET; NUMBER "1"; RSBRA; COMMA; LITERAL "footer1"; ENDLINE;
+            LSBRA; CARET; LITERAL "Eric"; RSBRA; COMMA; LITERAL "type";EQUAL; WHITESPACE 1;
                 LITERAL "Website"; COMMA; LITERAL "author"; EQUAL; WHITESPACE 1;
                 LITERAL "Eric"; WHITESPACE 1; LITERAL "Wang"; COMMA; LITERAL "title";
                 EQUAL; WHITESPACE 1; LITERAL "Not a real website"; COMMA; LITERAL "year";
                 EQUAL; WHITESPACE 1; NUMBER "2017"; COMMA; LITERAL "url"; EQUAL;
                 WHITESPACE 1; LITERAL "www.example.com/website"; COMMA;
                 LITERAL "access"; EQUAL; WHITESPACE 1; NUMBER "2018"; MINUS; NUMBER "3";
-                MINUS; NUMBER "4"; ENDLINE],
-            [Paragraph [[FrmtedString (Literal "text1#text2")]];
-                Header ({HeaderName = [FrmtedString (Literal "Header1")]; Level = 1;},"Header10");
-                Paragraph [[FrmtedString (Literal "text3");Reference(Literal "1","1"); FrmtedString (Literal "text4")]];
-                Paragraph [
-                    [Reference(Literal "1","1");FrmtedString (Literal ",footer1")];
-                    [FrmtedString (Literal "text5");Reference (Literal "(Wang, 2017)","Eric"); FrmtedString (Literal "text6")]];
-                Footnote (1,[FrmtedString (Literal "footer1")]);
-                Citation (
+                MINUS; NUMBER "4"; ENDLINE]
+    let refStyleToks = [PERCENT; PERCENT; LITERAL "Style"; EQUAL; WHITESPACE 1; LITERAL "Chicago"; ENDLINE]
+    let refPobjs =
+        [
+            Footnote (1,[FrmtedString (Literal "footer1")]);
+            Citation (
                     "Eric",Literal "(Wang, 2017)",
                     [FrmtedString (Literal "Eric Wang. "); FrmtedString (Literal "2017. ");
                     FrmtedString (Literal "\"Not a real website.\" ");
                     FrmtedString (Literal "Accessed March 4, 2018. ");
                     Link (Literal "www.example.com/website","www.example.com/website")]
-                )
-            ]
-            |> Ok, "Inherited big test"
-        )
-    ]        
+            )]
+    makeExpectoTestList id makeOk parse "top level reference tests" [
+        (
+            refStyleToks
+            @[LSBRA; CARET; NUMBER "1"; RSBRA]
+            @refToks,
+            [Paragraph [[Reference (Literal "1","1")]]]@refPobjs,
+            "just 1 footnote"
+        );
+        (
+            refStyleToks
+            @[LSBRA; CARET; LITERAL "Eric"; RSBRA]
+            @refToks,
+            [Paragraph [[Reference (Literal "(Wang, 2017)","Eric")]]]@refPobjs,
+            "just 1 citation"
+        );
+        (
+            refStyleToks
+            @[LITERAL "text3"; LSBRA; CARET; NUMBER "1"; RSBRA; LITERAL "text4"]
+            @refToks,
+            [Paragraph [[FrmtedString (Literal "text3");Reference (Literal "1","1"); FrmtedString (Literal "text4")]]]@refPobjs,
+            "Literal and 1 footnote"
+
+        );
+        (
+            refStyleToks
+            @[LITERAL "text5"; LSBRA; CARET; LITERAL "Eric"; RSBRA; LITERAL "text6"]
+            @refToks,
+            [Paragraph [[FrmtedString (Literal "text5");Reference (Literal "(Wang, 2017)","Eric"); FrmtedString (Literal "text6")]]]@refPobjs,
+            "Literal and 1 citation"
+        );
+        (
+            refStyleToks
+            @[LITERAL "text3"; LSBRA; CARET; NUMBER "1"; RSBRA; LITERAL "text4";ENDLINE]
+            @[LITERAL "text5"; LSBRA; CARET; LITERAL "Eric"; RSBRA; LITERAL "text6"]
+            @refToks,
+            [Paragraph [
+                    [FrmtedString (Literal "text3");Reference (Literal "1","1"); FrmtedString (Literal "text4")];
+                    [FrmtedString (Literal "text5");Reference (Literal "(Wang, 2017)","Eric"); FrmtedString (Literal "text6")]
+                ]]@refPobjs,
+            "multiline, 1 footnote, 1 citation"
+        );
+    ]
 
 
 [<Tests>]
