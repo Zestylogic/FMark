c34667b8d4ce64d65259ccadfef5e568012683b2
ThunderMikey <mikecyj25@gmail.com>
2018-03-22 12:25:02 +0000

    fix issues in markdowngen

--- FMark/src/Common/Parser/ParserHelperFuncs.fs
+++ FMark/src/Common/Parser/ParserHelperFuncs.fs
@@ -12,7 +12,7 @@ type TEmphasis = UNDER | STAR // underscore and asterisk
 
 type ParagraphState = {Par: Token list; ReToks: Token list; ParMatched: bool}
 
-type FormatStyle = STRONG | EM | SEM
+type FormatStyle = STRONG | EM | SEM | STRIKE
 
 /// delete leading ENDLINEs and retur the rest
 let rec deleteLeadingENDLINEs toks =
@@ -160,6 +160,7 @@ let (|MatchTemplate|_|) strongOrEmOrBoth toks =
         | STRONG -> DASTERISK, DUNDERSCORE
         | EM -> ASTERISK, UNDERSCORE
         | SEM -> TASTERISK, TUNDERSCORE
+        | STRIKE -> DTILDE, DTILDE
     let attachInlineEle front back = Option.map (fun (x,y) -> x,y,front,back)
     match toks with
     | WHITESPACE _:: whatSym:: WHITESPACE _:: _ when whatSym=underscoreFormatter -> None      // not em
@@ -224,6 +225,11 @@ let (|MatchStrong|_|) toks =
 let (|MatchStrongAndEm|_|) toks =
     match toks with
     | MatchTemplate SEM result -> Some result
+    | _ -> None
+
+let (|MatchStrike|_|) toks =
+    match toks with
+    | MatchTemplate STRIKE result -> Some result
     | _ -> None
 
 /// match new paragraph sequence
@@ -437,6 +443,10 @@ let parseInLineElements2 refLst toks =
             , rtks
         | MatchEm (content, rtks, frontLiteral, backLiteral) ->
             let inlineContent = (parseInLines [] content |> Emphasis |> FrmtedString)
+            genFormat (currentLine, inlineContent, frontLiteral, backLiteral)
+            , rtks
+        | MatchStrike (content, rtks, frontLiteral, backLiteral) ->
+            let inlineContent = (parseInLines [] content |> Strike |> FrmtedString)
             genFormat (currentLine, inlineContent, frontLiteral, backLiteral)
             , rtks
         | MatchLink (hyperTextToks, urlToks, rtks) ->
