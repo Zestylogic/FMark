449e0c0818f7df7c36fe97a89bafefcbd4c35d8c
George Punter <gsp14@ic.ac.uk>
2018-03-22 19:52:31 +0000

    Separating Reference into Cite and Foot. Done htmlgen and parser changes. Need to change tests and generate js

--- FMark/src/Common/Types.fs
+++ FMark/src/Common/Types.fs
@@ -29,7 +29,9 @@ and InlineElement =
     | FrmtedString of TFrmtedString
     | Link of HyperText: TFrmtedString * URL: string
     | Picture of Alt: string * URL: string
-    | Reference of HyperText: TFrmtedString * ID: string
+    | Cite of HyperText: TFrmtedString * ID: string
+    | Foot of HyperText: TFrmtedString * ID: string
+
 type TLine = InlineElement list
 
 type THeader = {HeaderName: TLine; Level: int}
